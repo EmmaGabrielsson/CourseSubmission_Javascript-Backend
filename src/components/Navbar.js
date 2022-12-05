7feb6800bdc899c8aed271123832768d9f5daa3c
Emma Gabrielsson <111791152+dreamworks0601@users.noreply.github.com>
2022-12-05 21:30:57 +0100

    some startup for the navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,81 @@
+import React /*, { useState, useRef, useEffect }*/ from "react";
+import "./navbar.css";
+import searchIcon from "../assets/search-icon.svg";
+
+function Navbar() {
+  return (
+    <div className="nav">
+      <h1>
+        movie<span className="time">time</span>
+      </h1>
+      <div>
+        <input
+          name="search"
+          placeholder="search movie"
+          className="search-input"
+          type="text"
+        ></input>
+        <label type="search movie" for="search">
+          <img src={searchIcon} alt="search icon"></img>
+        </label>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
+
+/*
+const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+  const toggleLinks = () => {
+    setShowLinks(!showLinks);
+  };
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = '0px';
+    }
+  }, [showLinks]);
+  return (
+    <nav>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <img src={logo} className='logo' alt='logo' />
+          <button className='nav-toggle' onClick={toggleLinks}>
+            <FaBars />
+          </button>
+        </div>
+        <div className='links-container' ref={linksContainerRef}>
+          <ul className='links' ref={linksRef}>
+            {links.map((link) => {
+              const { id, url, text } = link;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <ul className='social-icons'>
+          {social.map((socialIcon) => {
+            const { id, url, icon } = socialIcon;
+            return (
+              <li key={id}>
+                <a href={url}>{icon}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
+*/
